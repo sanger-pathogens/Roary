f113f5f6cae1dd5612ef88a6cd2a8b83962b1fd1
andrewjpage <andrewjpage@gmail.com>
2013-04-19 10:22:45 +0100

    tests for parallel all against all blast

--- lib/Bio/PanGenome/ParallelAllAgainstAllBlast.pm
+++ lib/Bio/PanGenome/ParallelAllAgainstAllBlast.pm
@@ -28,9 +28,9 @@ has 'job_runner'              => ( is => 'ro', isa => 'Str',      default  => 'L
 has 'blast_results_file_name' => ( is => 'ro', isa => 'Str',      lazy => 1, builder => '_build_blast_results_file_name' );
 has 'makeblastdb_exec'        => ( is => 'ro', isa => 'Str',      default => 'makeblastdb' );
 has 'blastp_exec'             => ( is => 'ro', isa => 'Str',      default => 'blastp' );
-has '_chunk_fasta_file_obj'   => ( is => 'ro', isa => 'ArrayRef', lazy => 1, builder => '_build__chunk_fasta_file_obj' );
+has '_chunk_fasta_file_obj'   => ( is => 'ro', isa => 'Bio::PanGenome::ChunkFastaFile', lazy => 1, builder => '_build__chunk_fasta_file_obj' );
 has '_sequence_file_names'    => ( is => 'ro', isa => 'ArrayRef', lazy => 1, builder => '_build__sequence_file_names' );
-has '_makeblastdb_obj'        => ( is => 'ro', isa => 'ArrayRef', lazy => 1, builder => '_build__makeblastdb_obj' );
+has '_makeblastdb_obj'        => ( is => 'ro', isa => 'Bio::PanGenome::External::Makeblastdb', lazy => 1, builder => '_build__makeblastdb_obj' );
 has '_blast_database'         => ( is => 'ro', isa => 'Str',      lazy => 1, builder => '_build__blast_database' );
 has '_job_runner_class'       => ( is => 'ro', isa => 'Str',      lazy => 1, builder => '_build__job_runner_class' );
 has '_working_directory' =>
@@ -64,7 +64,7 @@ sub _build__chunk_fasta_file_obj {
 
 sub _build__sequence_file_names {
     my ($self) = @_;
-    $self->_chunk_fasta_file_obj->sequence_file_names;
+    return $self->_chunk_fasta_file_obj->sequence_file_names;
 }
 
 sub _build__working_directory_name {
@@ -106,9 +106,9 @@ sub run {
         push( @expected_output_files, $output_seq_results_file );
         push( @commands_to_run,       $blast_database->_command_to_run() );
     }
-    my $job_runner_obj = $self->_job_runner_class->new( commands_to_run => @commands_to_run );
+    my $job_runner_obj = $self->_job_runner_class->new( commands_to_run => \@commands_to_run );
     $job_runner_obj->run();
-    $self->_combine_blast_results(@expected_output_files);
+    $self->_combine_blast_results(\@expected_output_files);
     return 1;
 }
 
