c35e71bfaeb063cb7f85db303e6ed15270003cc0
andrewjpage <andrewjpage@gmail.com>
2013-04-26 08:46:00 +0100

    only look at a fasta file once when building the single pan genome

--- lib/Bio/PanGenome/Output/OneGenePerGroupFasta.pm
+++ lib/Bio/PanGenome/Output/OneGenePerGroupFasta.pm
@@ -36,25 +36,46 @@ sub _build__groups {
     return \@groups;
 }
 
-sub _lookup_sequence {
-    my ( $self, $gene, $filename ) = @_;
+sub _lookup_sequences {
+    my ( $self, $genes, $filename ) = @_;
+    my @sequences;
+    
+    my %gene_queue;
+    for my $gene (@{$genes})
+    {
+      $gene_queue{$gene}++;
+    }
+    
 
     my $fasta_obj = Bio::SeqIO->new( -file => $filename, -format => 'Fasta' );
     while ( my $seq = $fasta_obj->next_seq() ) {
-        next unless ( $seq->display_id eq $gene );
-        return $seq;
+        for my $gene ( keys %gene_queue)
+        {
+          next unless ( $seq->display_id eq $gene );
+          push(@sequences, $seq);
+        }
     }
-    return undef;
+    return \@sequences;
 }
 
 sub create_file {
     my ($self) = @_;
 
+    my %filenames_to_genes;
     for my $group ( @{ $self->_groups } ) {
         my $gene = $self->analyse_groups->_groups_to_genes->{$group}->[0];
-        my $seq = $self->_lookup_sequence( $gene, $self->analyse_groups->_genes_to_file->{$gene} );
-        next unless ( defined($seq) );
+        
+        push(@{$filenames_to_genes{ $self->analyse_groups->_genes_to_file->{$gene}}},$gene);
+    }
+    
+    for my $filename (keys %filenames_to_genes)
+    {
+        my $sequences = $self->_lookup_sequences( $filenames_to_genes{$filename}, $filename );
+        next unless ( $sequences );
+        for my $seq (@{ $sequences})
+        {
         $self->_output_seq_io->write_seq($seq);
+      }
     }
 
     1;
