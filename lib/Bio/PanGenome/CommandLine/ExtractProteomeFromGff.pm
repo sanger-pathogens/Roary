606621c8808af70260683a84f617213f542d29e8
andrewjpage <andrewjpage@gmail.com>
2013-09-24 12:11:13 +0100

    copy and paste syntax error

--- lib/Bio/PanGenome/CommandLine/ExtractProteomeFromGff.pm
+++ lib/Bio/PanGenome/CommandLine/ExtractProteomeFromGff.pm
@@ -15,7 +15,7 @@ use File::Basename;
 
 
 has 'args'        => ( is => 'ro', isa => 'ArrayRef', required => 1 );
-has 'script_name' =bin/extract_proteome_from_gff> ( is => 'ro', isa => 'Str',      required => 1 );
+has 'script_name' => ( is => 'ro', isa => 'Str',      required => 1 );
 has 'help'        => ( is => 'rw', isa => 'Bool',     default  => 0 );
 
 has 'gff_files'      => ( is => 'rw', isa => 'ArrayRef' );
