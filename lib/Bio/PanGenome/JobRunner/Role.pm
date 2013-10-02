0924fa412316c86bf4449c32df6914779393a67a
andrewjpage <andrewjpage@gmail.com>
2013-10-02 11:44:06 +0100

    run post analysis as a job

--- lib/Bio/PanGenome/JobRunner/Role.pm
+++ lib/Bio/PanGenome/JobRunner/Role.pm
@@ -15,6 +15,7 @@ has 'job_runner'              => ( is => 'rw', isa => 'Str',  default  => 'Local
 has '_job_runner_class'       => ( is => 'ro', isa => 'Str',  lazy => 1, builder => '_build__job_runner_class' );
 has '_memory_required_in_mb'  => ( is => 'rw', isa => 'Int',  default => '200' );
 has '_queue'                  => ( is => 'rw', isa => 'Str',  default => 'normal' );
+has 'dont_wait'               => ( is => 'rw', isa => 'Bool', default => 0 );
 
 sub _build__job_runner_class {
     my ($self) = @_;
