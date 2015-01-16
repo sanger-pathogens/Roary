782a3c88f2a1ae8cc9e9940cd4862e88ca42ae5e
Carla Cummins <cc21@sanger.ac.uk>
2015-01-16 16:04:50 +0000

    Added QC reporting modules

---
+++ lib/Bio/PanGenome/QC/Report.pm
@@ -0,0 +1,62 @@
+package Bio::PanGenome::QC::Report;
+
+# ABSTRACT: generate a report based on kraken output
+
+=head1 SYNOPSIS
+
+=cut
+
+use Moose;
+use File::Temp;
+use Cwd;
+use Bio::PanGenome::QC::ShredAssemblies;
+
+has 'fasta_assemblies' => ( is => 'ro', isa => 'ArrayRef', required => 1 );
+has 'kraken_exec'      => ( is => 'ro', isa => 'Str',      default => 'kraken' );
+has 'kraken_db'        => ( is => 'ro', isa => 'Str',      default => '' );
+has 'outfile'          => ( is => 'rw', isa => 'Str',      default => 'qc_report.csv' );
+has '_kraken_data'     => ( is => 'rw', isa => 'ArrayRef', lazy_build => 1 );
+has '_header'          => ( is => 'rw', isa => 'Str',      lazy_build => 1 );
+has '_tmp_directory'   => ( is => 'rw', isa => 'Str',      lazy_build => 1 );
+
+sub _build__kraken_data {
+	my $self = shift;
+
+	my $shredder = Bio::PanGenome::QC::ShredAssemblies->new(
+		assembly_files   => $self->fasta_assemblies,
+		output_directory => $self->_tmp_directory
+	);
+	$shredder->shred or die ( "Failed to shred assembly data\n" );
+
+	my $kraken = Bio::PanGenome::QC::Kraken->new(
+		assembly_directory => $self->_tmp_directory,
+	);
+	return $kraken->top_hits;
+}
+
+sub _build__header {
+	return join( ',', ( 'Sample', 'Genus', 'Species' ) );
+}
+
+sub _build__tmp_directory {
+	my $temp_directory_obj = File::Temp->newdir(DIR => getcwd, CLEANUP => 1 );
+	my $tmp = $temp_directory_obj->dirname();
+
+	return $tmp;
+}
+
+sub report {
+	my $self = shift;
+
+	open( OUTFILE, '>', $self->outfile );
+	print OUTFILE $self->_header . "\n";
+	for my $line ( @{ $self->_kraken_data } ){
+		print OUTFILE join( ',', @{ $line } ) . "\n";
+	}
+	close OUTFILE;
+}
+
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+1;
---
+++ lib/Bio/PanGenome/QC/ShredAssemblies.pm
@@ -0,0 +1,68 @@
+package Bio::PanGenome::QC::ShredAssemblies;
+
+# ABSTRACT: slice .fa assemblies into "reads" for kraken input
+
+=head1 SYNOPSIS
+
+=cut
+
+use Moose;
+use Bio::SeqIO;
+use File::Basename;
+use Cwd;
+
+use Data::Dumper;
+
+has 'assembly_files'   => ( is => 'ro', isa => 'ArrayRef', required => 1 );
+has 'read_size'        => ( is => 'rw', isa => 'Int',      default => 150 );
+has 'output_directory' => ( is => 'rw', isa => 'Str',      lazy_build => 1 );
+
+sub _build_output_directory {
+	return getcwd;
+}
+
+sub shred {
+	my $self = shift;
+
+	foreach my $assembly ( @{ $self->assembly_files } ){
+		# read seq and shred into reads
+		my $seqio = Bio::SeqIO->new( -file => $assembly, -format => 'fasta' );
+
+		my @reads;
+		my $seq;
+		while( $seq = $seqio->next_seq() ){
+			push( @reads, @{ $self->_shredded_seq($seq->{primary_seq}->{seq}) } );
+		}
+
+		# write to file
+		my $prefix = basename( $assembly, ".fa" );
+		my $outfile = $self->output_directory . "/$prefix.shred.fa";
+		open( OUTFH, '>',  $outfile ) or die "Couldn't open $outfile: $!";
+		my $c = 1;
+		foreach my $r ( @reads ){
+			print OUTFH ">" . $prefix . "_$c\n";
+			print OUTFH "$r\n";
+			$c++;
+		}
+		close OUTFH;
+	}
+	1;
+}
+
+sub _shredded_seq {
+	my ( $self, $seq ) = @_;
+	chomp $seq;
+
+	my $size = $self->read_size;
+	my $unpack = "(A$size)*";
+	my @reads = unpack( $unpack, $seq );
+
+	my $last = pop @reads;
+	push( @reads, $last ) unless $last eq ''; # deal with trailing empty entries
+
+	return \@reads;
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+1;
