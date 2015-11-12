3f15758829ed1ab6bc54ed2a8bfa6b994da3362f
andrewjpage <andrewjpage@gmail.com>
2015-11-12 13:51:21 +0000

    check for job runner

--- lib/Bio/Roary/CommandLine/CreatePanGenome.pm
+++ lib/Bio/Roary/CommandLine/CreatePanGenome.pm
@@ -50,6 +50,10 @@ Options: -p INT    number of threads [1]
 Example: Quickly generate a core gene alignment using 16 threads
 
          bsub.py --threads 16 10 log create_pan_genome -e --mafft -p 16  *.gff
+         
+Example: Allow Roary to bsub the jobs to LSF - you cant bsub this command itself
+
+         create_pan_genome -j LSF -e --mafft -p 16  *.gff
 		 
 Example: Create a tree and visualise with iCANDY
 
